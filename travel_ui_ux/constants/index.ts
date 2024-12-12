dabe4af2c9f2bf10b3eb3edfc8347afec2fc55c6
roshanadh57 <76510189+roshanadh57@users.noreply.github.com>
2024-12-12 12:10:34 +0545

    Add files via upload

---
+++ travel_ui_ux/constants/index.ts
@@ -0,0 +1,86 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: '/', key: 'home', label: 'Home' },
+  { href: '/', key: 'how_hilink_work', label: 'How Durga Travels work?' },
+  { href: '/', key: 'services', label: 'News Blog' },
+  // { href: '/', key: 'pricing ', label: 'Pricing ' },
+  { href: '/', key: 'contact_us', label: 'Contact Us' },
+];
+
+// CAMP SECTION
+export const PEOPLE_URL = [
+  '/person-1.png',
+  '/person-2.png',
+  '/person-3.png',
+  '/person-4.png',
+];
+
+// FEATURES SECTION
+export const FEATURES = [
+  {
+    title: 'Set an tour to different country',
+    icon: '/map.svg',
+    variant: 'green',
+    description:
+      'We provide a solution for you to be able to use our website for traveling and ticketing',
+  },
+  {
+    title: 'Set an adventure schedule',
+    icon: '/calendar.svg',
+    variant: 'green',
+    description:
+      "Schedule an adventure with friends. On holidays, there are many interesting offers from Hilink. That way, there's no more discussion",
+  },
+  {
+    title: 'Technology using augment reality',
+    icon: '/tech.svg',
+    variant: 'green',
+    description:
+      'Technology uses augmented reality as a guide to your hiking trail in the forest to the top of the mountain. Already supported by the latest technology without an internet connection',
+  },
+  {
+    title: 'Many new locations every month',
+    icon: '/location.svg',
+    variant: 'orange',
+    description:
+      'Lots of new locations every month, because we have a worldwide community of climbers who share their best experiences with climbing',
+  },
+];
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: 'Learn More',
+    links: [
+      'About Durga Travels',
+      'Newsletter',
+      'Environment',
+      'Jobs',
+      'Privacy Policy',
+      'Contact Us',
+    ],
+  },
+  {
+    title: 'Our Community',
+    links: ['Malyasia Tour', 'Europe Tour', 'Thailand Tour'],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: 'Contact Us',
+  links: [
+    { label: 'Admin Officer', value: '9876543212' },
+    { label: 'Email Officer', value: 'durgatravels@durgatavels.com' },
+  ],
+};
+
+export const SOCIALS = {
+  title: 'Social',
+  links: [
+    '/facebook.svg',
+    '/instagram.svg',
+    '/twitter.svg',
+    '/youtube.svg',
+    '/wordpress.svg',
+  ],
+};
